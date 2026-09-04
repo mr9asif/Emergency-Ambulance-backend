5158c62400110dce1eb8ef0233cda4a8e25ba5be
mr9asif <asifalibd002@gmail.com>
2026-09-04 15:40:28 +0600

    database connecte

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import app from "./app.js";
+import { prisma } from "./lib/prisma.js";
 
 const port = 5000;
 
-app.listen(port, () => {
-  console.log("server running on port", port);
-});
+const main = async () => {
+  try {
+    await prisma.$connect();
+    console.log("database connect successfully");
+
+    app.listen(port, () => {
+      console.log("server running on port", port);
+    });
+  } catch (error) {
+    console.log(error);
+    prisma.$disconnect();
+    process.exit(1);
+  }
+};
+
+main();
