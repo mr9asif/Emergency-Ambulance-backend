73b745f38dcc8dec320a2f4221d97df54f65a56f
mr9asif <asifalibd002@gmail.com>
2026-09-19 09:19:55 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("server running well");
 });
 
